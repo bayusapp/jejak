7bb954b55290818b9b90e6557d845c76a5059768
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2026-07-21 17:39:40 +0700

    add routes landing page and beranda for role owner

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,4 +3,13 @@
 use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'LandingPage::index');
+
+$routes->group('Auth', function ($routes) {
+  $routes->get('', 'Auth::index');
+  $routes->get('cek', 'Auth::cek');
+});
+
+$routes->group('Owner', function ($routes) {
+  $routes->get('Beranda', 'Owner\Beranda::index');
+});
